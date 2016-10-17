670c57ff52f618bff70894b8eda58d498a4f747b
danielS081486 <teslavortex@gmail.com>
2016-10-16 18:18:53 -0700

    Added end game logic

--- main.js
+++ main.js
@@ -1,84 +1,112 @@
 
-var offset = 10;
-var amount = 3;
-var blockSize = 100;
+var offset     = 10;
+var amount     = 3;
+var blockSize  = 100;
 
 
 
-var r = 0;
-var g = 0;
-var b = 0;
+var block_id   = 0;
+var grid       = [];
 
-var block_id = 0;
-var grid = [];
-
-
-//var myVar = setInterval(myTimer, 1000);
-
-//function myTimer(){
-
-	for(i = 0; i< amount; i++){
+var turn       = 0;
+var winner     = 0;
 
 
+for(i = 0; i< amount; i++){
+	for(j = 0; j< amount; j++){
+
+		var div = document.createElement('div');
+		div.style.boxSizing = 'border-box';
+		div.style.border    = '1px solid black';
+		div.style.position  = 'absolute';
+		div.style.display   = 'inline-block';
+
+
+		div.style.left      = (i*blockSize) + offset;
+		div.style.top       = (j*blockSize) + offset;
+
+		div.style.width     = (blockSize).toString()+'px';
+		div.style.height    = (blockSize).toString()+'px';
 		
 
-		for(j = 0; j< amount; j++){
-			console.log(block_id);
+		div.addEventListener('click', playerMove);
 
-			r = Math.floor(Math.random() * 256);
-			g = Math.floor(Math.random() * 256);
-			b = Math.floor(Math.random() * 256);
+		div.setAttribute('class', 'block');
+		div.block_id        = block_id;
+		div.state           = 0;
 
+		//div.innerText = block_id;
 
+		grid.push(div);
 
+		document.body.appendChild(div);
 
-			var div = document.createElement('div');
-			div.style.boxSizing = 'border-box';
-			div.style.border= '1px solid black';
-			div.style.position = 'absolute';
-			div.style.display = 'inline-block';
-			div.style.padding = '0';
-			div.style.margin  = '0';
-
-			div.style.left = (i*blockSize) + offset;
-			div.style.top = (j*blockSize) + offset;
-
-			div.style.width= (blockSize).toString()+'px';
-			div.style.height= (blockSize).toString()+'px';
-			//div.style.background= 'rgb('+ (r).toString() +','+ (g).toString() +','+ (b).toString() +')';
-			
-
-			div.addEventListener('click', boom);
-
-			div.setAttribute('class', 'block');
-			div.block_id = block_id;
-			div.state = 0;
-
-			grid.push(div);
-
-			document.body.appendChild(div);
-
-			block_id++;
-
-		}
-	}
-//}
-
-
-
-function boom(){
-
-	if(grid[this.block_id].state == 0){
-		//grid[this.block_id].style.background= '#fff';
-		//grid[this.block_id].style.color = '#000';
-		grid[this.block_id].innerText= 'X';
-		grid[this.block_id].state = 1;
-	}else{
-		//grid[this.block_id].style.background= '#000';
-	//	grid[this.block_id].style.color = '#f00';
-		grid[this.block_id].innerText= 'O';
-		grid[this.block_id].state = 0;
+		block_id++;
 
 	}
-	//console.log(grid[this.block_id].style.background);
 }
+
+
+
+function playerMove(){
+
+	if(grid[this.block_id].state == 0 && turn == 0 && winner == 0){
+		turn = 1;
+		grid[this.block_id].innerText = '.X';
+		grid[this.block_id].state = 1;
+
+		check();
+		computerMove();
+
+
+	}
+
+
+}
+
+
+
+function computerMove(){
+
+
+
+
+	check();
+	turn = 0;
+}
+
+
+
+
+
+function check(){
+	if(Math.abs((grid[0].state + grid[3].state + grid[6].state)) == 3){
+		winner = grid[0].state;
+
+	}else if(Math.abs((grid[1].state + grid[4].state + grid[7].state)) == 3){
+		winner = grid[1].state;
+
+	}else if(Math.abs((grid[2].state + grid[5].state + grid[8].state)) == 3){
+		winner = grid[2].state;
+
+	}else if(Math.abs((grid[0].state + grid[1].state + grid[2].state)) == 3){
+		winner = grid[0].state;
+
+	}else if(Math.abs((grid[3].state + grid[4].state + grid[5].state)) == 3){
+		winner = grid[3].state;
+
+	}else if(Math.abs((grid[6].state + grid[7].state + grid[8].state)) == 3){
+		winner = grid[6].state;
+
+	}else if(Math.abs((grid[0].state + grid[4].state + grid[8].state)) == 3){
+		winner = grid[0].state;
+
+	}else if(Math.abs((grid[2].state + grid[4].state + grid[6].state)) == 3){
+		winner = grid[2].state;
+
+	}
+
+}
+
+
+
